1435e243ed36f09fa8348d58ccd94976dc864d7b
Erick Malaguez Rowedder <erickmalaguezrowedder@gmail.com>
2023-05-22 12:08:01 -0300

    Ajustes finais

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-const btnClass = document.querySelectorAll('.btn-class') // Const para botões
-const nSelected = document.querySelector('#n-selected') // Const para os números selecionados
-const sendBtn = document.querySelector('#send-btn') // Const para o submit 
-const tela1 = document.querySelector('.main-container') // Const para o container onde você seleciona o número
-const tela2 = document.querySelector('.thanks-container') // Const para o container que mostra o número selecionado
-let selecionado
+const btnClass = document.querySelectorAll('.btn-class');
+const nSelected = document.querySelector('#n-selected');
+const sendBtn = document.querySelector('#send-btn');
+const tela1 = document.querySelector('.main-container');
+const tela2 = document.querySelector('.thanks-container');
+let selected;
 
-function addOrRemoveSelectedClass(btn) { // Função de adicionar ou remover a classe selected
-    if (btn.classList.contains('selecionado')) {
-        btn.classList.remove('selecionado');
+function addOrRemoveSelectedClass(nota) { 
+    var btn = document.querySelector("#nota" + nota);
+    if (btn.classList.contains(selected)) {
+        btn.classList.remove(selected);
     } else {
-        console.log(selecionado)
-        if (selecionado) {
-            selecionado.classList.remove('selecionado');
+        if (selected) {
+            selected.classList.remove("selected");
         }
-        btn.classList.add('selecionado');
-        selecionado = btn 
+        btn.classList.add("selected");
+        selected = btn;
+        nSelected.innerHTML = nota;
     }
 }
 
 function addOrRemoveSendBtn(btn) {
+    sendBtn.disabled = true;
     if (nSelected.innerHTML = btn.innerHTML) {
         sendBtn.disabled = false;
     } else {
@@ -26,15 +28,11 @@ function addOrRemoveSendBtn(btn) {
     }
 }
 
-sendBtn.disabled = true; // Começa true para que o usuário não possa dar submit sem antes selecionar o número
-btnClass.forEach((btn) => {
-    btn.addEventListener("click", () => { // Assim que clicado
-        nSelected.innerHTML = btn.innerHTML // ... O campo da mensagem (que é um span) recebe o valor do botão (O número selecionado)
-        sendBtn.disabled = false; // ... O campo de submit passa a ter o disabled false, podendo assim ser usado.
-    })
-})
+function display() { 
+    tela1.style.display = "none"; 
+    tela2.style.display = "block"; 
+}
 
-sendBtn.addEventListener("click", () => { // Assim que o submit é usado
-    tela1.style.display = "none" // ... A tela 1 fica com o display none
-    tela2.style.display = "block" // E a tela 2 aparece, com o display block.
-})
+
+
+
